783bbde1b7dc87f9d7133169ea1ce8b1c858f8e6
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-12-04 01:55:52 +0000

    make mathybperf a module.

---
+++ mathybperf/__init__.py
@@ -0,0 +1,15 @@
+from mathybperf.setup.auxiliary import *
+from mathybperf.setup.form import *
+from mathybperf.setup.mesh import *
+from mathybperf.setup.parameters import *
+from mathybperf.setup.solver import *
+from mathybperf.setup.space import *
+
+from mathybperf.utils.analytics import *
+from mathybperf.utils.fem_utils import *
+from mathybperf.utils.petsc_utils import *
+from mathybperf.utils.plotting import *
+from mathybperf.utils.result_utils import *
+
+from mathybperf.verification.error import *
+from mathybperf.verification.geometric import *
