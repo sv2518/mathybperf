7cbe1873204b8825359dd8f4ea20b5ea5dc0198b
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-12-06 22:55:11 +0000

    update verfication.

--- mathybperf/verification/error.py
+++ mathybperf/verification/error.py
@@ -9,3 +9,14 @@ def check_error(w, w2):
     assert np.allclose(sigma.dat.data, sigma2.dat.data, atol=1.e-2), norm
     norm = errornorm(u, u2, norm_type="L2")
     assert np.allclose(u.dat.data, u2.dat.data, atol=1.e-2), norm
+
+def get_errors(w, w2):
+    linf_err_u = max(abs(assemble(w.sub(0) - w2.sub(0)).dat.data))
+    linf_err_p = max(abs(assemble(w.sub(1) - w2.sub(1)).dat.data))
+    l2_err_u = errornorm(w.sub(0), w2.sub(0), "L2")
+    l2_err_p = errornorm(w.sub(1), w2.sub(1), "L2")
+    hdiv_err_u = errornorm(w.sub(0), w2.sub(0), "hdiv")
+    h1_err_p = errornorm(w.sub(1), w2.sub(1), "H1")
+    return ((linf_err_u, linf_err_p),
+            (l2_err_u, l2_err_p),
+            (hdiv_err_u, h1_err_p))
