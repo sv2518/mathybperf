fcb24a1c6a562cda94afa1520ee33c09ef2ccf68
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-12-13 16:11:17 +0100

    refactor mesh

--- mathybperf/setup/mesh.py
+++ mathybperf/setup/mesh.py
@@ -1,16 +1,21 @@
 from firedrake import *
 
 
-def mesh_3D(cells_per_dim, s, deform, affine, quadrilateral):
+def mesh_3D(cells_per_dim, s, deform, affine, quadrilateral, hierarchy_nlevels=None):
     n = cells_per_dim
-    mesh = SquareMesh(n, n, s, quadrilateral=quadrilateral)
-    mesh = ExtrudedMesh(mesh, n)
+    base = SquareMesh(n, n, s, quadrilateral=quadrilateral)
+    if hierarchy_nlevels:
+        basemh = MeshHierarchy(base, hierarchy_nlevels)
+        mh = ExtrudedMeshHierarchy(basemh, s, base_layer=n)
+        mesh = mh[-1]
+    else:
+        mesh = ExtrudedMesh(base, n)
     coords = mesh.coordinates.dat.data
 
-    if not affine:
+    if not affine and deform:
         mesh.coordinates.dat.data[2][1] += deform * mesh.coordinates.dat.data[2][1]
         mesh.coordinates.dat.data[4][1] += deform * 0.25 * mesh.coordinates.dat.data[4][1]
-    else:
+    elif affine and deform:
         for i in range(coords.shape[0]):
             if coords[i][2] > 0:
                 mesh.coordinates.dat.data[i][2] *= deform
