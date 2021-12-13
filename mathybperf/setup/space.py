49f4c4ebd5740b77f2d0fa712a08994ee47fcf91
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-12-13 16:14:00 +0100

    refactor space

--- mathybperf/setup/space.py
+++ mathybperf/setup/space.py
@@ -8,8 +8,8 @@ def RT_DQ_3D(p, mesh):
     CG = FiniteElement("CG", interval, p+1)
     HDiv_ele = EnrichedElement(HDiv(TensorProductElement(RT, DG_v)),
                                HDiv(TensorProductElement(DG_h, CG)))
-    V = FunctionSpace(mesh, HDiv_ele)
-    U = FunctionSpace(mesh, "DQ", p)
-    W = V * U
+    U = FunctionSpace(mesh, HDiv_ele)
+    V = FunctionSpace(mesh, "DQ", p)
+    W = U * V
 
     return W, U, V
