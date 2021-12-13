1ea3c8bc44d87ab87a38009510d5127060896419
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-12-13 16:13:20 +0100

    Double check solutions are solutions to our variational form

--- mathybperf/setup/solver.py
+++ mathybperf/setup/solver.py
@@ -16,6 +16,37 @@ def solve_with_params(a, L, W, solver_bag, deform, penalty_value, quadrature_deg
     with PETSc.Log.Event("perfsolve"):
         solver.solve()
     return w, solver
+    
+    sigma_dc, u_dc = TrialFunctions(W)
+    tau_dc, v_dc = TestFunctions(W)
+    _, U_dc = W.split()
+    f_dc = Function(U_dc)
+    x = SpatialCoordinate(solver_bag.mesh)
+    w_dc = Function(W).assign(w)
+    assert w_dc != w, "Make sure we don't modify w"
+    f_dc.interpolate(-2*(x[0]-1)*x[0] - 2*(x[1]-1)*x[1]- 2*(x[2]-1)*x[2])
+    w_dc.sub(1).project(x[0]*(1-x[0])*x[1]*(1-x[1])*x[2]*(1-x[2]))
+    A=Tensor((dot(sigma_dc, tau_dc) + div(tau_dc)*u_dc + div(sigma_dc)*v_dc)*dx)
+    B=AssembledVector(w_dc)
+    dat1 = assemble(A*B).dat.data[1]
+    dat2 = assemble(-f_dc*v_dc*dx).dat.data[1]
+    assert np.allclose(dat1, dat2), "Analytical solution is not correct"
+    
+    sigma_dc, u_dc = TrialFunctions(W)
+    tau_dc, v_dc = TestFunctions(W)
+    _, U_dc = W.split()
+    f_dc = Function(U_dc)
+    x = SpatialCoordinate(solver_bag.mesh)
+    f_dc.interpolate(-2*(x[0]-1)*x[0] - 2*(x[1]-1)*x[1]- 2*(x[2]-1)*x[2])
+    w_dc.sub(1).project(x[0]*(1-x[0])*x[1]*(1-x[1])*x[2]*(1-x[2]))
+    A=Tensor((dot(sigma_dc, tau_dc) + div(tau_dc)*u_dc + div(sigma_dc)*v_dc)*dx)
+    B=AssembledVector(w)
+    dat1 = assemble(A*B).dat.data[1]
+    dat2 = assemble(-f_dc*v_dc*dx).dat.data[1]
+    assert np.allclose(dat1, dat2), "Iterative solution is not correct"
+
+    assert np.allclose(w_dc.dat.data[0], w.dat.data[0], rtol=1.e-4)
+    assert np.allclose(w_dc.dat.data[1], w.dat.data[1], rtol=1.e-4)
 
 
 def naive_solver(a, L, W, solver_bag):
