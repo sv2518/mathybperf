315ba5e52efe3d083bfad4c924968f5b80976cdc
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-01-07 18:03:06 +0100

    Genearate and publish flamegraphs automatically

--- mathybperf/performance/run_profiler.sh
+++ mathybperf/performance/run_profiler.sh
@@ -2,6 +2,26 @@
 
 # turn off threading
 export OMP_NUM_THREADS=1
+
+# mode of the script, options are:
+# do we want to generate a tex from this?
+# do we want to generate new results?
+ARG1="$1"
+ARG2="$2"
+if [[ "$ARG1" == "--nores" || "$ARG2" == "--nores" ]]
+then
+    DORES=false
+else
+    DORES=true
+fi
+if [[ "$ARG1" == "--tex" || "$ARG2" == "--tex" ]]
+then
+    DOTEX=true
+else
+    DOTEX=false
+fi
+
+# setup
 ORDERS=(0)
 LEVELS=2
 SCALING=(1)
@@ -14,8 +34,6 @@ FLAME=true
 PERFORMP='perform_params'
 BASEP='baseline_params'
 
-echo $ORDERS
-
 # setup output folder name
 # first choose a case name
 CASE='/case1/'
@@ -23,7 +41,9 @@ FOLDER='results/mixed_poisson/'
 FOLDER+='pplus1pow3/'  # penalty set permanently to this
 TRAFOTYPE='trafo_'$TRAFO
 NAME=$FOLDER$TRAFOTYPE$CASE
+FLAMENAME='flames/mixed_poisson/pplus1pow3/'$TRAFOTYPE$CASE
 mkdir -p $NAME
+mkdir -p $FLAMENAME
 
 # file name is parameter set name
 for D in $DEFORM
@@ -34,7 +54,7 @@ do
         do
             for C in $CELLSPD
             do
-                if not $FLAME
+                if ! $FLAME
                 then 
                     FLARG=''
                 fi
@@ -42,40 +62,71 @@ do
                 # run base case
                 PARAMS=$BASEP
                 NNAME=$NAME$PARAMS
+                FNAME=$FLAMENAME$PARAMS
                 firedrake-clean
                 NNAME+='_warm_up'
+                FNAME+='_warm_up'
                 if $FLAME
                 then
-                    FLARG='-log_view :flame_'$NNAME'.txt:ascii_flamegraph'
+                    FLARG='-log_view :'$FNAME'_flame.txt:ascii_flamegraph'
                 fi
                 python3 run_profiler.py $NNAME $PARAMS $P $LEVELS $QUADS $S $D $TRAFO $C $FLARG --add_to_quad_degree "${ATQD[@]}" --clean
-                
-                NNAME+='_warmed_up'
                 if $FLAME
                 then
-                    FLARG='-log_view :flame_'$NNAME'.txt:ascii_flamegraph'
+                ../../../FlameGraph/flamegraph.pl $FNAME"_flame.txt" > $FNAME"_flame.html"  --inverted --title "Firedrake example" --countname us --fontsize 13 --colors "eyefriendly"
                 fi
-                python3 run_profiler.py $NNAME $PARAMS $P $LEVELS $QUADS $S $D $TRAFO $C $FLARG --add_to_quad_degree "${ATQD[@]}" 
 
+                NNAME=$NAME$PARAMS
+                FNAME=$FLAMENAME$PARAMS
+                NNAME+='_warmed_up'
+                FNAME+='_warmed_up'
+                if $FLAME
+                then
+                    FLARG='-log_view :'$FNAME'_flame.txt:ascii_flamegraph'
+                fi
+                python3 run_profiler.py $NNAME $PARAMS $P $LEVELS $QUADS $S $D $TRAFO $C $FLARG --add_to_quad_degree "${ATQD[@]}"
+                if $FLAME
+                then
+                ../../../FlameGraph/flamegraph.pl $FNAME"_flame.txt" > $FNAME"_flame.html"  --inverted --title "Firedrake example" --countname us --fontsize 13 --colors "eyefriendly"
+                fi
 
                 # run perf case
                 PARAMS=$PERFORMP
                 NNAME=$NAME$PARAMS
+                FNAME=$FLAMENAME$PARAMS
                 firedrake-clean
                 NNAME+='_warm_up'
+                FNAME+='_warm_up'
                 if $FLAME
                 then
-                    FLARG='-log_view :flame_'$NNAME'.txt:ascii_flamegraph'
+                    FLARG='-log_view :'$FNAME'_flame.txt:ascii_flamegraph'
                 fi
                 python3 run_profiler.py $NNAME $PARAMS $P $LEVELS $QUADS $S $D $TRAFO $C $FLARG --add_to_quad_degree "${ATQD[@]}" --clean
-                
-                NNAME+='_warmed_up'
                 if $FLAME
                 then
-                    FLARG='-log_view :flame_'$NNAME'.txt:ascii_flamegraph'
+                ../../../FlameGraph/flamegraph.pl $FNAME"_flame.txt" > $FNAME"_flame.html"  --inverted --title "Firedrake example" --countname us --fontsize 13 --colors "eyefriendly"
                 fi
-                python3 run_profiler.py $NNAME $PARAMS $P $LEVELS $QUADS $S $D $TRAFO $C $FLARG --add_to_quad_degree "${ATQD[@]}" 
+
+                NNAME=$NAME$PARAMS
+                FNAME=$FLAMENAME$PARAMS
+                NNAME+='_warmed_up'
+                FNAME+='_warmed_up'
+                if $FLAME
+                then
+                    FLARG='-log_view :'$FNAME'_flame.txt:ascii_flamegraph'
+                fi
+                python3 run_profiler.py $NNAME $PARAMS $P $LEVELS $QUADS $S $D $TRAFO $C $FLARG --add_to_quad_degree "${ATQD[@]}"
+                if $FLAME
+                then
+                ../../../FlameGraph/flamegraph.pl $FNAME"_flame.txt" > $FNAME"_flame.html"  --inverted --title "Firedrake example" --countname us --fontsize 13 --colors "eyefriendly"
+                fi
             done
         done
     done
 done
+
+# Make new flamegraphs online accessible
+git add $FLAMENAME*"_flame.html"
+git commit -m "New flamegraphs were generated for parameter sets "$BASEP" and "$PERFORMP"."
+CURRENT_BRANCH=$(git branch --show-current)
+git push origin $CURRENT_BRANCH
