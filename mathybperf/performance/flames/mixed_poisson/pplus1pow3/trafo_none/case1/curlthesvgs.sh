ce225afa88236e6c1d556524c12026a7d3885526
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-01-07 18:58:39 +0100

    New script to fetch flamegraphs was generated.

--- mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/curlthesvgs.sh
+++ mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/curlthesvgs.sh
@@ -1,4 +1,4 @@
-\#!/bin/sh
+#!/bin/sh
 mkdir -p ./svgs/flames/mixed_poisson/pplus1pow3/trafo_none/case1/
  cd svgs
 curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/baseline_params_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3/trafo_none/case1/baseline_params_warm_up_flame.svg
