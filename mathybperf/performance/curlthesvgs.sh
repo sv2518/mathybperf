dfb838f97f629eb3ff656629b1778bb5a65abdc2
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-01-11 15:57:16 +0100

    New script to fetch flamegraphs was generated.

--- mathybperf/performance/curlthesvgs.sh
+++ mathybperf/performance/curlthesvgs.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 mkdir -p ./svgs/
 cd svgs
-mkdir -p flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_0/cells_3/
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_0/cells_3/jacks_baseline_params_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_0/cells_3/jacks_baseline_params_warm_up_flame.svg
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_0/cells_3/jacks_baseline_params_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_0/cells_3/jacks_baseline_params_warmed_up_flame.svg
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_0/cells_3/perform_params_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_0/cells_3/perform_params_warm_up_flame.svg
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_0/cells_3/perform_params_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_0/cells_3/perform_params_warmed_up_flame.svg
-mkdir -p flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_1/cells_3/
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_1/cells_3/jacks_baseline_params_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_1/cells_3/jacks_baseline_params_warm_up_flame.svg
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_1/cells_3/jacks_baseline_params_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_1/cells_3/jacks_baseline_params_warmed_up_flame.svg
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_1/cells_3/perform_params_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_1/cells_3/perform_params_warm_up_flame.svg
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_1/cells_3/perform_params_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3/trafo_none/case1/order_1/cells_3/perform_params_warmed_up_flame.svg
 
 
