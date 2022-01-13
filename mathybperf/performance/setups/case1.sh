d5ae77344221c9fbd331cc45b9cf7e53395dc2c3
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-01-13 16:22:47 +0100

    Case1: LU vs Jacks GTMG

--- mathybperf/performance/setups/case1.sh
+++ mathybperf/performance/setups/case1.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export ORDERS=(0 1 2)
+# This run just compares how Jacks GTMG on the trace solve compare to lu on the trace solves
+export ORDERS=(0 1 2 3)
 export LEVELS=2
 export SCALING=(1)
 export DEFORM=(0)
@@ -9,8 +10,8 @@ export ATQD=(0 0)
 export CELLSPD=(3)
 export QUADS=true
 export FLAME=true
-export PERFORMP='perform_params'
-export BASEP='jacks_baseline_params'
+export BASEP='hybridization_lu_params'
+export PERFORMP='gtmg_matexpl_params'
 export SOLTYPE='quadratic'
 export PROJECTEXACTSOL="" #--projectexactsol
 export CASE='/case1/'
