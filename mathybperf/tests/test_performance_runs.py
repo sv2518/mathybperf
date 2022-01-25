89ea45841e7dc923648e8dec8f1a845d35e2c114
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-01-25 17:56:31 +0100

    Run all setups

--- mathybperf/tests/test_performance_runs.py
+++ mathybperf/tests/test_performance_runs.py
@@ -36,6 +36,6 @@ def run_profiler(name):
     assert returncode==0, "Case "+name+" failed. Error message in file "+str(error_file)+": \n"+error_message
 
 
-@pytest.mark.parametrize("name", ["case4"])
+@pytest.mark.parametrize("name", setups)
 def test_setups_mixed_poisson(name):
     run_profiler(name)
