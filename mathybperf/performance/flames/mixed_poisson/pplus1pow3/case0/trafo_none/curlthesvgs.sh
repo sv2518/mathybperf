1cddc69c5e98f587968bd4d32c92c5f30009892f
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-02-28 14:05:46 +0100

    New script to fetch flamegraphs was generated.

--- mathybperf/performance/flames/mixed_poisson/pplus1pow3/case0/trafo_none/curlthesvgs.sh
+++ mathybperf/performance/flames/mixed_poisson/pplus1pow3/case0/trafo_none/curlthesvgs.sh
@@ -2,8 +2,8 @@
 mkdir -p ./svgs/flames/mixed_poisson/pplus1pow3//case0/trafo_none/
 cd svgs
 mkdir -p flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_fully_matfree_cg_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_fully_matfree_cg_warm_up_flame.svg
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_fully_matfree_cg_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_fully_matfree_cg_warmed_up_flame.svg
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_cg_params_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_cg_params_warm_up_flame.svg
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_cg_params_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_cg_params_warmed_up_flame.svg
 curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_fully_matfree_cg_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_fully_matfree_cg_warm_up_flame.svg
 curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_fully_matfree_cg_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3//case0/trafo_none/order_0/cells_3/hybridization_fully_matfree_cg_warmed_up_flame.svg
 
