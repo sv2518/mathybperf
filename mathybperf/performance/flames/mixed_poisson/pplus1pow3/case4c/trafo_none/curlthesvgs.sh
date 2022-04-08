e4dc758af2868269e8a8219b1cbbb77fd9d30d92
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-04-08 15:03:50 +0200

    New script to fetch flamegraphs was generated.

--- mathybperf/performance/flames/mixed_poisson/pplus1pow3/case4c/trafo_none/curlthesvgs.sh
+++ mathybperf/performance/flames/mixed_poisson/pplus1pow3/case4c/trafo_none/curlthesvgs.sh
@@ -2,8 +2,8 @@
 mkdir -p ./svgs/flames/mixed_poisson/pplus1pow3//case4c/trafo_none/
 cd svgs
 mkdir -p flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_matexpl_nested_schur_params_chebynone_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_matexpl_nested_schur_params_chebynone_warm_up_flame.svg
-curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_matexpl_nested_schur_params_chebynone_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_matexpl_nested_schur_params_chebynone_warmed_up_flame.svg
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_global_matfree_params_matexpmg_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_global_matfree_params_matexpmg_warm_up_flame.svg
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_global_matfree_params_matexpmg_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_global_matfree_params_matexpmg_warmed_up_flame.svg
 curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_fully_matfree_params_matexpmg_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_fully_matfree_params_matexpmg_warm_up_flame.svg
 curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_fully_matfree_params_matexpmg_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3//case4c/trafo_none/order_0/cells_3/gtmg_fully_matfree_params_matexpmg_warmed_up_flame.svg
 
