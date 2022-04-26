440179f3a8f1cbe4f164ef5176758833ecfb6f12
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-04-26 17:23:58 +0200

    More params and cases

--- mathybperf/performance/setups/case5b.sh
+++ mathybperf/performance/setups/case5b.sh
@@ -2,7 +2,7 @@
 
 # This run just compares how Jacks GTMG on the trace solve compare in global matfree mode
 # compare to the same setup just with including locally matrix-free stuff too
-export ORDERS=(0 1 2 3 4)
+export ORDERS=(0 1 2 3)
 export LEVELS=2
 export SCALING=(1)
 export DEFORM=(0)
@@ -12,7 +12,7 @@ export CELLSPD=(3)
 export QUADS=true
 export FLAME=true
 export BASEP='gtmg_global_matfree_params_matexpmg_assembledjacobi_fgmres'
-export PERFORMP='gtmg_fully_matfree_params_matexpmg_assembledjacobi_fs0_cg_jacobi_fgmres'
+export PERFORMP='gtmg_fully_matfree_params_matexpmg_assembledjacobi_fs0_cg_jacobi_fgmres_moretol'
 export SOLTYPE='quadratic'
 export PROJECTEXACTSOL="" #--projectexactsol
-export CASE='/case5/'
+export CASE='/case5b/'
--- mathybperf/performance/setups/case7b.sh
+++ mathybperf/performance/setups/case7b.sh
@@ -2,17 +2,17 @@
 
 # This run just compares how Jacks GTMG on the trace solve compare in global matfree mode
 # compare to the same setup just with including locally matrix-free stuff too
-export ORDERS=(0 1 2 3 4)
+export ORDERS=(0 1 2 3)
 export LEVELS=2
 export SCALING=(1)
 export DEFORM=(0)
 export TRAFO='none' # 'affine'
 export ATQD=(0 0)
-export CELLSPD=(3)
+export CELLSPD=(2)
 export QUADS=true
 export FLAME=true
 export BASEP='gtmg_global_matfree_params_matexpmg_assembledjacobi_fgmres'
-export PERFORMP='gtmg_fully_matfree_params_matexpmg_assembledjacobi_fs0_cg_jacobi_fgmres'
+export PERFORMP='gtmg_fully_matfree_params_matexpmg_assembledjacobi_fs0_cg_jacobi_fgmres_moretol'
 export SOLTYPE='quadratic'
 export PROJECTEXACTSOL="" #--projectexactsol
-export CASE='/case5/'
+export CASE='/case7b/'
