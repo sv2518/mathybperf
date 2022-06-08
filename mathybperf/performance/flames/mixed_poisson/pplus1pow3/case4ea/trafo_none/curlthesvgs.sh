a420fde235ce7a17997cdabfe9e6db1d496935b7
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-06-08 02:49:40 +0200

    New script to fetch flamegraphs was generated.

---
+++ mathybperf/performance/flames/mixed_poisson/pplus1pow3/case4ea/trafo_none/curlthesvgs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir -p ./svgs/flames/mixed_poisson/pplus1pow3//case4ea/trafo_none/
+cd svgs
+mkdir -p flames/mixed_poisson/pplus1pow3//case4ea/trafo_none/order_4/cells_2/
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case4ea/trafo_none/order_4/cells_2/gtmg_global_matfree_params_matexpmg_assembledjacobi_fgmres_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3//case4ea/trafo_none/order_4/cells_2/gtmg_global_matfree_params_matexpmg_assembledjacobi_fgmres_warm_up_flame.svg
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case4ea/trafo_none/order_4/cells_2/gtmg_global_matfree_params_matexpmg_assembledjacobi_fgmres_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3//case4ea/trafo_none/order_4/cells_2/gtmg_global_matfree_params_matexpmg_assembledjacobi_fgmres_warmed_up_flame.svg
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case4ea/trafo_none/order_4/cells_2/gtmg_fully_matfree_params_fgmres_coarselu_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3//case4ea/trafo_none/order_4/cells_2/gtmg_fully_matfree_params_fgmres_coarselu_warm_up_flame.svg
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case4ea/trafo_none/order_4/cells_2/gtmg_fully_matfree_params_fgmres_coarselu_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3//case4ea/trafo_none/order_4/cells_2/gtmg_fully_matfree_params_fgmres_coarselu_warmed_up_flame.svg
+
+
