87e1d59f699488fcf2a705351c3b6736578dca4c
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-06-13 10:19:46 +0200

    Add test for gtmg_fully_matfree_params_fs0_cg_jacobi_fs1_cg_laplacian_jacobi_fgmres_outerfgmres_dosingleit_withassjacobi

---
+++ mathybperf/performance/setups/case6e.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export ORDERS=(0 1 2 3 4)
+export LEVELS=2
+export SCALING=(1)
+export DEFORM=(0)
+export TRAFO='none' # 'affine'
+export ATQD=(0 0)
+export CELLSPD=(2)
+export QUADS=true
+export FLAME=true
+export BASEP='gtmg_global_matfree_params_matexpmg_assembledjacobi_fgmres'
+export PERFORMP='gtmg_fully_matfree_params_fs0_cg_jacobi_fs1_cg_laplacian_jacobi_fgmres_outerfgmres_dosingleit_withassjacobi'
+export SOLTYPE='quadratic'
+export PROJECTEXACTSOL="" #--projectexactsol
+export CASE='/case6e/'
