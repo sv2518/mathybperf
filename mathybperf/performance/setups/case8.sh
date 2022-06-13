3e6c9dfc1990a8078fa2bb0574afc079aaa77e83
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-06-13 10:19:07 +0200

    Fix case8

--- mathybperf/performance/setups/case8.sh
+++ mathybperf/performance/setups/case8.sh
@@ -2,13 +2,13 @@
 
 # This run just compares how Jacks GTMG on the trace solve compare in global matfree mode
 # compare to the same setup just with including locally matrix-free stuff too
-export ORDERS=(0 1 2 3)
-export LEVELS=0
+export ORDERS=(5)
+export LEVELS=2
 export SCALING=(1)
 export DEFORM=(0)
 export TRAFO='none' # 'affine'
 export ATQD=(0 0)
-export CELLSPD=(3)
+export CELLSPD=(2)
 export QUADS=true
 export FLAME=true
 export BASEP='fgmres_jacobi'
