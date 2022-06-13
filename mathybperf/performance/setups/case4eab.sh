26b7525ab307afdd3dbbf0a2c1dc572ab37c7583
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-06-13 10:23:48 +0200

    Add a test that compares to lu on coarse level

---
+++ mathybperf/performance/setups/case4eab.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Same as 4c but with assembled jacobi as smoother
+export ORDERS=(0 1 2 3 4)
+export LEVELS=2
+export SCALING=(1)
+export DEFORM=(0)
+export TRAFO='none' # 'affine'
+export ATQD=(0 0)
+export CELLSPD=(2)
+export QUADS=true
+export FLAME=true
+export BASEP='gtmg_global_matfree_params_matexpmg_assembledjacobi_fgmres'
+export PERFORMP='gtmg_fully_matfree_params_fgmres_coarselu'
+export SOLTYPE='quadratic'
+export PROJECTEXACTSOL="" #--projectexactsol
+export CASE='/case4eab/'
--- mathybperf/setup/parameters.py
+++ mathybperf/setup/parameters.py
@@ -630,6 +630,36 @@ gtmg_fully_matfree_params_fgmres = {'snes_type': 'ksponly',
                                                       'ksp_converged_reason': None}}
 
 
+gtmg_fully_matfree_params_fgmres_coarselu = {'snes_type': 'ksponly',
+                                             'mat_type': 'matfree',
+                                             'ksp_type': 'fgmres',
+                                             'ksp_converged_reason': None,
+                                             'ksp_rtol': 1.e-6,
+                                             'ksp_atol': 1.e-6,
+                                             'ksp_max_it': 2,
+                                             'pc_type': 'python',
+                                             'pc_python_type': 'firedrake.HybridizationPC',
+                                             'hybridization': {'ksp_type': 'fgmres',
+                                                               'pc_type': 'python',
+                                                               'mat_type': 'matfree',
+                                                               'ksp_rtol': 1.e-6,
+                                                               'ksp_atol': 1.e-6,
+                                                               'localsolve': {'ksp_type': 'preonly',
+                                                                              'mat_type': 'matfree',  # local-matfree!
+                                                                              'pc_type': 'fieldsplit',
+                                                                              'pc_fieldsplit_type': 'schur',
+                                                                              'approx': False,
+                                                                              'fieldsplit_0': {'ksp_rtol': 1.e-16,
+                                                                                               'ksp_atol': 1.e-16},
+                                                                              'fieldsplit_1': {'ksp_atol': 1.e-12,
+                                                                                               'ksp_rtol': 1.e-12}},
+                                                               'pc_python_type': 'firedrake.GTMGPC',
+                                                               'gt': gt_params_fully_matfree_lu_assembledjacobi,
+                                                               'ksp_view': None,
+                                                               'ksp_monitor': None,
+                                                               'ksp_converged_reason': None}}
+
+
 gtmg_fully_matfree_params_matexpmg = {'snes_type': 'ksponly',
                                       'mat_type': 'matfree',
                                       'ksp_type': 'fgmres',
