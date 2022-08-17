33c2360d809de3e5c3296e526e48187df5834b0a
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-08-17 15:09:14 +0200

    Updates on investigation

--- mathybperf/investigation/run_investigation.py
+++ mathybperf/investigation/run_investigation.py
@@ -4,31 +4,33 @@ from mathybperf import *
 from mathybperf.utils.solver_utils import SolverBag
 import os
 
-# TODO
-# 1) fix counting of iterations
-# 2) fix condition number plots
-
 
 def run_test(deformations, orders, penalty, affine_trafo, add_to_quad_degree, params, name, type):
     dofs = np.zeros(len(orders))
-    konditionnumberlist = []
+    konditionnumberlist_mixed = []
+    konditionnumberlist_A00 = []
+    konditionnumberlist_Schur = []
     eigenvaluelist = []
     outeritslist = []
     totalitslist = []
-    results_outer_its = np.zeros([len(orders)])
-    results_total_its = np.zeros([len(orders)])
+    fsp0itslist = []
+    fsp1itslist = []
 
     for deform in deformations:
-        konditionnumbers = []
+        konditionnumbers_mixed = []
+        konditionnumbers_Schur = []
+        konditionnumbers_A00 = []
+        results_outer_its = []
+        results_total_its = []
+        results_fsp0_its = []
+        results_fsp1_its = []
         eigenvals = []
-        totalits = []
-        outerits = []
         quaddegreelist = []
+        s = deform if type == "scaling" else 1
+        deform = 1 if type == "scaling" else deform
         for i, p in enumerate(orders):
             try:
                 # Problem setup and solve
-                s = deform if type == "scalings" else 1
-                deform = 1 if type == "scalings" else deform
                 problem_bag = ProblemBag(deform, s, affine_trafo, True,
                                          p, add_to_quad_degree, penalty, 1, "quadratic")
                 solver_bag = SolverBag(params, None, None)
@@ -42,31 +44,33 @@ def run_test(deformations, orders, penalty, affine_trafo, add_to_quad_degree, pa
                 analytics_mixed = Analytics(a)
                 _, wmin, wmax = analytics_mixed.eigenvalues()
                 eigenvals.append((wmin, wmax))
-                konditionnumbers.append(analytics_mixed.condition_number())
+                konditionnumbers_mixed.append(analytics_mixed.condition_number())
                 # print("Mixed poisson spd?", analytics_mixed.spd())
                 # print("Mixed poisson singular?", analytics_mixed.singular())
-                print("Mixed poisson condition number", analytics_mixed.condition_number())
 
-                # AB = Tensor(a).blocks
-                # S = AB[1, 1] - AB[1, 0] * AB[0, 0].inv * AB[0, 1]
-                # analytics_schur = Analytics(S)
+                AB = Tensor(a).blocks
+                S = AB[1, 1] - AB[1, 0] * AB[0, 0].inv * AB[0, 1]
+                analytics_schur = Analytics(S)
+                konditionnumbers_Schur.append(analytics_schur.condition_number())
                 # print("Schur spd?", analytics_schur.spd())
                 # print("Schur singular?", analytics_schur.singular())
-                # print("Schur condition number", analytics_schur.condition_number())
 
-                # analytics_vmass = Analytics(AB[0, 0])
+                analytics_vmass = Analytics(AB[0, 0])
+                konditionnumbers_A00.append(analytics_vmass.condition_number())
                 # print("Velocity mass spd?", analytics_vmass.spd())
                 # print("Velocity mass singular?", analytics_vmass.singular())
-                # print("Velocity mass condition number", analytics_vmass.condition_number())
 
                 # Iteration counts
                 outer_its = solver.snes.ksp.its
                 pcs = solver.snes.ksp.pc.getFieldSplitSubKSP()
                 fsp0_its = pcs[0].its
                 fsp1_its = pcs[1].its
-                results_total_its[i] = outer_its*(fsp1_its+fsp1_its*fsp0_its)
-                results_outer_its[i] = outer_its
+                results_total_its.append(outer_its*(fsp1_its+fsp1_its*fsp0_its))
+                results_outer_its.append(outer_its)
+                results_fsp0_its.append(fsp0_its)
+                results_fsp1_its.append(fsp1_its)
 
+                # Visualisation of solutions on the deformed cells
                 # velo, pres = w.split()
                 # f1 = File("mathybperf/investigation/visualisation/poisson_mixed_velocity_"+str(p)+".pvd")
                 # f1.write(velo)
@@ -76,64 +80,52 @@ def run_test(deformations, orders, penalty, affine_trafo, add_to_quad_degree, pa
             except Exception as e:
                 # check that aborted bc of divergence
                 if solver.snes.diverged or solver.snes.ksp.diverged or pcs[0].diverged or pcs[1].diverged:
-                    results_total_its[i] = -1
-                    results_outer_its[i] = -1
+                    results_total_its.append(-1)
+                    results_outer_its.append(-1)
+                    results_fsp0_its.append(-1)
+                    results_fsp1_its.append(-1)
                 else:
-                    results_total_its[i] = -2
-                    results_outer_its[i] = -2
+                    results_total_its.append(-2)
+                    results_outer_its.append(-2)
+                    results_fsp0_its.append(-2)
+                    results_fsp1_its.append(-2)
 
-            outerits.append(results_outer_its[i])
-            totalits.append(results_total_its[i])
             dofs[i] = solver.snes.vec_sol.local_size
             quaddegreelist.append(list(int(q) for q in quadrature_degree))
 
-        outeritslist.append(outerits)
-        totalitslist.append(totalits)
-        konditionnumberlist.append(konditionnumbers)
+        outeritslist.append(results_outer_its)
+        totalitslist.append(results_total_its)
+        fsp0itslist.append(results_fsp0_its)
+        fsp1itslist.append(results_fsp1_its)
+        konditionnumberlist_mixed.append(konditionnumbers_mixed)
+        konditionnumberlist_Schur.append(konditionnumbers_Schur)
+        konditionnumberlist_A00.append(konditionnumbers_A00)
         eigenvaluelist.append(eigenvals)
 
     # write data to csv file
-    write_investigation_data([eigenvaluelist, konditionnumberlist,
-                              outeritslist, totalitslist, deformations],
+    data_per_defo = [eigenvaluelist, konditionnumberlist_mixed,
+                     konditionnumberlist_A00, konditionnumberlist_Schur,
+                     outeritslist, totalitslist,
+                     fsp0itslist, fsp1itslist,
+                     deformations]
+    names_per_defo = ["eigs",
+                      "kond_mixed", "kond_A00", "kond_Schur",
+                      "outer_its", "total_its",
+                      "deformations"]
+    write_investigation_data(data_per_defo,
                              name+"_per_deformation.csv",
-                             ["eigs", "kond", "outer_its", "total_its",
-                              "deformations"],
-                             ["deformation from 1.0 in percentage : %d" % (d*100) for d in deformations])
-    write_investigation_data([orders, dofs, quaddegreelist, [params]*len(orders)],
+                             names_per_defo,
+                             ["deformation from 1.0 in percentage : %d"
+                              % (d*100) for d in deformations])
+
+    data_per_order = [orders, dofs, quaddegreelist, [params]*len(orders)]
+    names_per_order = ["orders", "dofs", "quad_degree", "params"]
+    write_investigation_data(data_per_order,
                              name+"_per_order.csv",
-                             ["orders", "dofs", "quad_degree", "params"])
+                             names_per_order)
 
 
 # !!!!
-class DGLaplacian(AuxiliaryOperatorPC):
-    def form(self, pc, u, v):
-        ctx = self.get_appctx(pc)
-        d = ctx["deform"]
-        value = ctx["value"]
-        q = ctx["quadrature_degree"]
-
-        def gamma(p, h, d, value):
-            return Constant(value)/(h*h)
-
-        W = u.function_space()
-        n = FacetNormal(W.mesh())
-
-        p = W._ufl_element._sub_elements[0]._degree
-        h = CellVolume(W.mesh())/FacetArea(W.mesh())
-
-        a_dg = -(dot(grad(v), grad(u))*dx(degree=q)
-                 - dot(grad(v), (u)*n)*ds_v(degree=q)
-                 - dot(v*n, grad(u))*ds_v(degree=q)
-                 + gamma(p, h, d, value)*dot(v, u)*ds_v(degree=q)
-                 - dot(grad(v), (u)*n)*ds_t(degree=q)
-                 - dot(v*n, grad(u))*ds_t(degree=q)
-                 + gamma(p, h, d, value)*dot(v, u)*ds_t(degree=q)
-                 - dot(grad(v), (u)*n)*ds_b(degree=q)
-                 - dot(v*n, grad(u))*ds_b(degree=q)
-                 + gamma(p, h, d, value)*dot(v, u)*ds_b(degree=q))
-
-        bcs = []
-        return (a_dg, bcs)
 
 
 params = {"mat_type": "matfree",
@@ -152,25 +144,24 @@ params = {"mat_type": "matfree",
           "fieldsplit_0_ksp_monitor": None,
           "fieldsplit_1_ksp_type": "cg",
           "fieldsplit_1_pc_type": "python",
-          "fieldsplit_1_pc_python_type": __name__ + ".DGLaplacian",
+          "fieldsplit_1_pc_python_type": DGLaplacian.__module__ + ".DGLaplacian",  # defined in utils/auxiliary
           "fieldsplit_1_ksp_rtol": 1e-8,
           "fieldsplit_1_ksp_atol": 1e-8,
           "fieldsplit_1_ksp_max_it": 2000,
           "fieldsplit_1_aux_pc_type": "jacobi",
           "fieldsplit_1_ksp_monitor": None,
           "ksp_monitor": None,
-          "ksp_norm_type": "unpreconditioned",
           }
 
 
-for type in ["nonaffine_defo"]:  # , "affine_defo", "scaling", "no_defo"]:
+for type in ["scaling"]:
     penalty = lambda p, d: (p+1)**3
     case = "/(p+1)**3/"
     test = "cgjacobi"
     degree = range(6)
     deformations = ([0.2*d for d in range(1, 26)]
-                    if type in ["nonaffine_defo", "affine_defo", "scaling"] else
-                    [1])
+                    if type in ["nonaffine_defo", "affine_defo"] else
+                    [0.1*d for d in range(1, 26)])
     affine_trafo = False if type == "nonaffine_defo" else True
 
     # setup plotting output
@@ -178,7 +169,7 @@ for type in ["nonaffine_defo"]:  # , "affine_defo", "scaling", "no_defo"]:
     type = ("nonaffine" if type == "nonaffine_defo" else
             "affine" if type == "affine_defo" else
             "scaling" if type == "scaling" else
-            "nodeform")
+            "none")
 
     # !!!!
     for c, add_to_quad_degree in enumerate([(i, i) for i in range(1)]):
