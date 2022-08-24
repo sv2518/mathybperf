3dae1dc6e4505d2453e858b43b437909fb5f66cf
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-08-24 16:43:43 +0200

    New flamegraphs were generated for parameter sets hybridization_cg_params and hybridization_global_matfree_cg.

--- mathybperf/performance/setups/case0.sh
+++ mathybperf/performance/setups/case0.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # This run just compares how Jacks GTMG on the trace solve compare to lu on the trace solves
-export ORDERS=(0)
+export ORDERS=(0 1 2 3 4 5)
 export LEVELS=2
 export SCALING=(1)
 export DEFORM=(0)
 export TRAFO='none' # 'affine'
 export ATQD=(0 0)
-export CELLSPD=(3)
+export CELLSPD=(2)
 export QUADS=true
 export FLAME=true
 export BASEP='hybridization_cg_params'
-export PERFORMP='hybridization_fully_matfree_cg'
+export PERFORMP='hybridization_global_matfree_cg'
 export SOLTYPE='quadratic'
 export PROJECTEXACTSOL="" #--projectexactsol
 export CASE='/case0/'
