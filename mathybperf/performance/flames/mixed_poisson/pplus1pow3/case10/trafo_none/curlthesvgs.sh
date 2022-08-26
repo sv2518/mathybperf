51bd1a7fcb0290a8604f1b16b570d65407e21dc4
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-08-26 14:09:12 +0200

    New script to fetch flamegraphs was generated.

---
+++ mathybperf/performance/flames/mixed_poisson/pplus1pow3/case10/trafo_none/curlthesvgs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir -p ./svgs/flames/mixed_poisson/pplus1pow3//case10/trafo_none/
+cd svgs
+mkdir -p flames/mixed_poisson/pplus1pow3//case10/trafo_none/order_0/cells_2/
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case10/trafo_none/order_0/cells_2/gtmg_global_matfree_params_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3//case10/trafo_none/order_0/cells_2/gtmg_global_matfree_params_warm_up_flame.svg
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case10/trafo_none/order_0/cells_2/gtmg_global_matfree_params_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3//case10/trafo_none/order_0/cells_2/gtmg_global_matfree_params_warmed_up_flame.svg
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case10/trafo_none/order_0/cells_2/hybridization_fully_matfree_cg_warm_up_flame.svg>flames/mixed_poisson/pplus1pow3//case10/trafo_none/order_0/cells_2/hybridization_fully_matfree_cg_warm_up_flame.svg
+curl https://raw.githubusercontent.com/sv2518/mathybperf/main/mathybperf/performance/flames/mixed_poisson/pplus1pow3//case10/trafo_none/order_0/cells_2/hybridization_fully_matfree_cg_warmed_up_flame.svg>flames/mixed_poisson/pplus1pow3//case10/trafo_none/order_0/cells_2/hybridization_fully_matfree_cg_warmed_up_flame.svg
+
+
