95eb77b50bc99ce587f7ad38f81d8bd1a6a72951
sv2518 <sv2518@ic.ac.uk>
2022-10-06 20:26:02 +0100

    Fix rerun cases

--- mathybperf/performance/rerun_cases.sh
+++ mathybperf/performance/rerun_cases.sh
@@ -1,31 +1,26 @@
 #!/bin/sh
 
-C='case11'
+C='case0'
 #sh ./run_profiler.sh $C 0
 
 C='case1'
-#sh ./run_profiler.sh $C 0
-
-C='case8'
-#sh ./run_profiler.sh $C 0
+sh ./run_profiler.sh $C 0
 
 C='case2'
-#sh ./run_profiler.sh $C 0
+sh ./run_profiler.sh $C 0
 
 C='case3'
+sh ./run_profiler.sh $C 0
+
+C='case8'
+sh ./run_profiler.sh $C 0
+
+C='case4e'
+sh ./run_profiler.sh $C 0
+
+C='case6'
 sh ./run_profiler.sh $C 0
 
 C='case5'
 sh ./run_profiler.sh $C 0
 
-C='case8'
-sh ./run_profiler.sh $C 0
-
-C='case11'
-sh ./run_profiler.sh $C 0
-
-C='case6c'
-sh ./run_profiler.sh $C 0
-
-C='case0'
-#sh ./run_profiler.sh $C 0
