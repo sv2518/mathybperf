e1be362ce634f30e5c63ee152bbd175e7671b12d
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2022-10-07 12:15:21 +0200

    Cleanup table plotter

--- mathybperf/performance/make_tables.py
+++ mathybperf/performance/make_tables.py
@@ -17,9 +17,9 @@ flames = './flames/mixed_poisson/pplus1pow3/'
 trafo = 'trafo_none/'
 cases = os.listdir(home)
 plot_cases = ['case3', 'case0', 'case8', 'case4e', 'case6', 'case1', 'case2']
-map_cases_experiments_to_thesis = {'case3':'case3', 'case0':'case2', 'case8':'case1', 'case4e':'case6',
-                                   'case6': 'case7', 'case1':'case4', 'case2':'case5', 'case5':'case7', 'case11':'case9',
-                                   'case12':'case10'
+map_cases_experiments_to_thesis = {'case3': 'case3', 'case0': 'case2', 'case8': 'case1', 'case4e': 'case6',
+                                   'case6': 'case7', 'case1': 'case4', 'case2': 'case5', 'case5': 'case7', 'case11': 'case9',
+                                   'case12': 'case10'
                                    }
 cellslist = [['cells_2'], ['cells_4']]
 for cells in cellslist:
@@ -52,12 +52,12 @@ for cells in cellslist:
                                 its_data_per_param_trace.update({param: {}})
                             if not param in its_data_per_param_outer.keys():
                                 its_data_per_param_outer.update({param: {}})
-                        
+
                             with open(file) as json_file:
                                 its_outer = json.load(json_file)['outer_its']
                                 if its_outer:
                                     its_data_per_param_outer[param][order[-1]] = its_outer
-                            
+
                             with open(file) as json_file:
                                 its_trace = json.load(json_file)['trace_its']
                                 if its_trace:
@@ -69,7 +69,7 @@ for cells in cellslist:
                             param = fr"{a}".split("/")[-1]
                             if not param in dof_data_per_param.keys():
                                 dof_data_per_param.update({param: {}})
-                        
+
                             with open(file) as texfile:
                                 data = int(texfile.readlines()[4].split("&")[3])
                                 if data:
@@ -92,7 +92,7 @@ for cells in cellslist:
                                     time = 0
                                 if time:
                                     time_data_per_param[param][order[-1]] = time
-            
+
             rows = index_names
             for (key_t, value_t) in time_data_per_param.items():
                 time_data[map_cases_experiments_to_thesis[case]] = time_data_per_param[key_t]
@@ -102,9 +102,9 @@ for cells in cellslist:
                 its_data_trace[map_cases_experiments_to_thesis[case]] = its_data_per_param_trace[key_i]
             for (key_i, value_i) in dof_data_per_param.items():
                 dof_data[map_cases_experiments_to_thesis[case]] = dof_data_per_param[key_i]
-                
+
     dofs = sorted(dof_data[map_cases_experiments_to_thesis['case3']].items())
-    xlables = [f"p={k}\nDOFS={v}" for k,v in dofs]
+    xlables = [f"p={k}\nDOFS={v}" for k, v in dofs]
     print(xlables)
 
     # save latex tables
@@ -112,13 +112,13 @@ for cells in cellslist:
     with open(table_filename, 'w') as convert_file:
         frame = pd.DataFrame(time_data).to_latex(index=True, index_names=rows)
         convert_file.write(frame)
-        fig = plt.subplots(figsize=(12,6))
-        im = sns.heatmap(pd.DataFrame({k: {k2: np.exp(v2) for k2,v2 in v.items()} for k, v in time_data.items()}, index=rows).sort_index(0).sort_index(1), linewidth=2, square=True,
-                        annot=True, fmt="5.1f", cbar=False, cmap="Reds", vmax=7500, annot_kws={"size":14})
+        fig = plt.subplots(figsize=(12, 6))
+        im = sns.heatmap(pd.DataFrame({k: {k2: np.exp(v2) for k2, v2 in v.items()} for k, v in time_data.items()}, index=rows).sort_index(0).sort_index(1), linewidth=2, square=True,
+                         annot=True, fmt="5.1f", cbar=False, cmap="Reds", vmax=7500, annot_kws={"size": 14})
 
         im = sns.heatmap(pd.DataFrame(time_data, index=rows).sort_index(0).sort_index(1), linewidth=2, square=True,
-                        cbar_kws={"orientation": "vertical", "extend":"max", 'label': 'log(runtime in seconds)'}, cmap="Reds", annot=False, 
-                        annot_kws={"size":14}, fmt="5.1f", robust=True, vmax=9.5)
+                         cbar_kws={"orientation": "vertical", "extend": "max", 'label': 'log(runtime in seconds)'}, cmap="Reds", annot=False,
+                         annot_kws={"size": 14}, fmt="5.1f", robust=True, vmax=9.5)
 
         im.set_xlim([0, 6])
         im.set_yticklabels(xlables)
@@ -127,10 +127,10 @@ for cells in cellslist:
         im2.set_aspect('equal')
         im2.set_ylim((0, 5))
         im2.set_xlim([1, 6])
-        im2.axvline(color='black', linewidth=4,x=4) # xy1=(3, 6.5), xy2=(3, -0.5))
-        im2.axvline(color='black', linewidth=4,x=6) # xy1=(5, 6.5), xy2=(5, -0.5))
+        im2.axvline(color='black', linewidth=4, x=4)  # xy1=(3, 6.5), xy2=(3, -0.5))
+        im2.axvline(color='black', linewidth=4, x=6)  # xy1=(5, 6.5), xy2=(5, -0.5))
         im2.set_xticks([2.5, 5, 7])
-        im2.tick_params(axis=u'both', which=u'both',length=0)
+        im2.tick_params(axis=u'both', which=u'both', length=0)
         im2.set_xticklabels(['Group1', 'Group 2', 'Group 3'])
 
         im.spines['left'].set_position(('data', 0))
@@ -148,9 +148,9 @@ for cells in cellslist:
     with open(table_filename, 'w') as convert_file:
         frame = pd.DataFrame(its_data_outer, index=rows).to_latex(index=True, index_names=rows)
         convert_file.write(frame)
-        fig = plt.figure(figsize=(12,6))
+        fig = plt.figure(figsize=(12, 6))
         im = sns.heatmap(pd.DataFrame(its_data_outer, index=rows).sort_index(0).sort_index(1), linewidth=2, square=True,
-                    cbar_kws={"orientation": "vertical", 'label': 'trace solver iterations'}, cmap="Blues", annot=True,  fmt='g', vmax=30, annot_kws={"size":14})
+                         cbar_kws={"orientation": "vertical", 'label': 'trace solver iterations'}, cmap="Blues", annot=True, fmt='g', vmax=30, annot_kws={"size": 14})
         im.set_xlim([0, 6])
         im.set_yticklabels(xlables)
 
@@ -158,12 +158,12 @@ for cells in cellslist:
         im2.set_aspect('equal')
         im2.set_ylim((0, 5))
         im2.set_xlim([1, 6])
-        im2.axvline(color='black', linewidth=4,x=4) # xy1=(3, 6.5), xy2=(3, -0.5))
-        im2.axvline(color='black', linewidth=4,x=6) # xy1=(5, 6.5), xy2=(5, -0.5))
+        im2.axvline(color='black', linewidth=4, x=4)  # xy1=(3, 6.5), xy2=(3, -0.5))
+        im2.axvline(color='black', linewidth=4, x=6)  # xy1=(5, 6.5), xy2=(5, -0.5))
         im2.set_xticks([2.5, 5, 7])
-        im2.tick_params(axis=u'both', which=u'both',length=0)
+        im2.tick_params(axis=u'both', which=u'both', length=0)
         im2.set_xticklabels(['Group1', 'Group 2', 'Group 3'])
-        
+
         im.spines['left'].set_position(('data', 0))
         sns.despine(left=True, bottom=True, ax=im2)
         labels = im2.get_xticklabels()
@@ -179,26 +179,26 @@ for cells in cellslist:
     with open(table_filename, 'w') as convert_file:
         frame = pd.DataFrame(its_data_trace, index=rows).to_latex(index=True, index_names=rows)
         convert_file.write(frame)
-        fig = plt.figure(figsize=(12,6))
+        fig = plt.figure(figsize=(12, 6))
         im = sns.heatmap(pd.DataFrame(its_data_trace, index=rows).sort_index(0).sort_index(1), linewidth=2, square=True,
-                    cbar_kws={"orientation": "vertical", 'label': 'trace solver iterations'}, cmap="Blues", annot=True,  fmt='g', vmax=390, annot_kws={"size":14})
+                         cbar_kws={"orientation": "vertical", 'label': 'trace solver iterations'}, cmap="Blues", annot=True, fmt='g', vmax=390, annot_kws={"size": 14})
         im.set_xlim([0, 6])
         im.set_yticklabels(xlables)
-        
+
         im2 = im.twiny()
         im2.set_aspect('equal')
         im2.set_ylim((0, 5))
         im2.set_xlim([1, 6])
-        im2.axvline(color='black', linewidth=4,x=4) # xy1=(3, 6.5), xy2=(3, -0.5))
-        im2.axvline(color='black', linewidth=4,x=6) # xy1=(5, 6.5), xy2=(5, -0.5))
+        im2.axvline(color='black', linewidth=4, x=4)  # xy1=(3, 6.5), xy2=(3, -0.5))
+        im2.axvline(color='black', linewidth=4, x=6)  # xy1=(5, 6.5), xy2=(5, -0.5))
         im2.set_xticks([2.5, 5, 7])
-        im2.tick_params(axis=u'both', which=u'both',length=0)
+        im2.tick_params(axis=u'both', which=u'both', length=0)
         im2.set_xticklabels(['Group1', 'Group 2', 'Group 3'])
-        
+
         im.spines['left'].set_position(('data', 0))
         sns.despine(left=True, bottom=True, ax=im2)
         labels = im2.get_xticklabels()
         for label in labels:
             label.set_fontweight('bold')
-        plt.tight_layout() 
+        plt.tight_layout()
         plt.savefig(f'./plots/table_its_{cells[0]}_trace.pdf')
